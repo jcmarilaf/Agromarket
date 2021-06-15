0b302d6704819686791ee80361c2301cf1437e76
Gustavo <gdolivah08@gmail.com>
2021-06-15 19:22:20 -0400

    Creación de la base de datos

--- AgroApp/models.py
+++ AgroApp/models.py
@@ -1,51 +1,109 @@
 from django.db import models
 from django.conf import settings
+from Usuario.models import Usuario
 
 # Create your models here.
 
 class Producto(models.Model):
-    ''' Descripción de la clase '''
+    ''' Dentro de esta clase podemos encontrar atributos tales como id_producto, nombre_producto, 
+    tipo_producto, precio_producto los cuales son obligatorios, y también algunos opcionales como 
+    descripción_producto y descuento_producto. Todo esto para lograr un correcto funcionamiento 
+    con todo lo relacionado a Producto '''
     codigo = models.CharField(max_length=10, unique=True)
     nombre = models.CharField(max_length=200)
     tipo_producto_opciones = (
-        ('', ''),
-        ('', ''),
-        ('', ''),
-        ('', ''),
-        ('', ''),
+        ('VERDURA', 'VERDURA'),
+        ('FRUTA', 'FRUTA'),
+        ('CARNE', 'CARNE'),
+        ('CEREALES', 'CEREALES'),
+        ('LIQUIDOS', 'LIQUIDOS'),
     )
     tipo_producto = models.CharField(max_length=10, choices=tipo_producto_opciones)
     precio = models.PositiveIntegerField()
     descripcion = models.CharField(max_length=1000, null=True)
     descuento = models.PositiveIntegerField(null=True)
+    usuario = models.ForeignKey(Usuario , related_name='productos', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['codigo']
+        verbose_name_plural = 'Productos'
+
+    def __str__(self):
+        return f'{self.codigo} - {self.nombre}'
 
 class Reporte(models.Model):
-    ''' A '''
+    ''' Aquí podemos ver atributos como id_reporte, razón_reporte y evidencia_reporte como opcional, 
+    de este modo el usuario al momento de utilizar la página podrá utilizar la opción de reportar. '''
     codigo = models.CharField(max_length=10, unique=True)
     razon = models.CharField(max_length=100)
     evidencia = models.ImageField(null=True)
     producto = models.ForeignKey(Producto, related_name='reportes', on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['codigo']
+        verbose_name_plural = 'Reportes'
+
+    def __str__(self):
+        return f'{self.codigo}'
+
 class ListaDeseos(models.Model):
-    ''' A '''
+    ''' En lista de deseos se encuentran los atributos id_lista e id_producto, 
+    pero este último es opcional, ya que cada usuario tiene acceso a una lista de deseos, 
+    pero no necesariamente deben tener productos agregados. '''
     codigo = models.CharField(max_length=10, unique=True)
-    producto = models.ForeignKey(Producto, related_name='reportes', on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, related_name='lista_deseos', on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, related_name='lista_deseos', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['codigo']
+        verbose_name_plural = 'Listas de deseos'
+
+    def __str__(self):
+        return f'{self.codigo}'
 
 
 class Comunidad(models.Model):
-    ''' A '''
+    ''' Los atributos que podemos visualizar en esta clase son id_comunidad y 
+    nombre_comunidad, los cuales serán necesarios para que el vendedor 
+    pueda crear de forma correcta una comunidad entre sus clientes. '''
     codigo = models.CharField(max_length=10, unique=True)
     nombre = models.CharField(max_length=100, unique=True)
+    usuario = models.ForeignKey(Usuario, related_name='comunidades', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['codigo']
+        verbose_name_plural = 'Comunidades'
+
+    def __str__(self):
+        return f'{self.codigo}'
 
 
 class Venta(models.Model):
-    ''' A '''
+    ''' Dentro de esta clase podemos encontrar atributos que están relacionados al momento en 
+    que se realiza una venta de un producto y sus atributos para el correcto funcionamiento 
+    los cuales son id_venta, id_producto y monto_venta. '''
     codigo = models.CharField(max_length=10, unique=True)
     monto_venta = models.PositiveIntegerField()
-    producto = models.ForeignKey(Producto, related_name='reportes', on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, related_name='ventas', on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, related_name='ventas', on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['codigo']
+        verbose_name_plural = 'Ventas'
+
+    def __str__(self):
+        return f'{self.codigo}'
 
 class HistorialCompra(models.Model):
-    ''' A '''
+    ''' Los atributos que incluye esta clase son id_historial e id_venta, 
+    y su finalidad es crear un historial de compra para el usuario en caso de
+    que este haya realizado alguna en algún momento. '''
     codigo = models.CharField(max_length=10, unique=True)
     venta = models.ForeignKey(Venta, related_name='historial_de_compras', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['codigo']
+        verbose_name_plural = 'Historiales de compra'
+
+    def __str__(self):
+        return f'{self.codigo}'
--- Usuario/models.py
+++ Usuario/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields import IntegerField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class TipoUsuario(models.Model):
+    idTipoUsuario = models.IntegerField(primary_key=True, verbose_name="Id de tipo de usuario")
+    nombreTipoUsuario = models.CharField(max_length=80, blank=False, null=False, verbose_name="Nombre de tipo usuario")
+
+
+class Usuario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    fono = models.CharField(max_length=12, blank=True, null=True,  verbose_name="Teléfono usuario")
+    direccion = models.CharField(max_length=100, null= True,  verbose_name="Dirección usuario")
+    descripcionPerfil = models.CharField(max_length=500, null= True,  verbose_name="Descripción perfil")
+    fotoPerfil = models.ImageField(default="sinfoto.jpg", null=False, blank=False,  verbose_name="Foto de perfil")
+    cantidadProductos = IntegerField(blank= True, null=True,  verbose_name="Cantidad de productos")
+    tipoUsuario = models.ForeignKey(TipoUsuario, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return str(self.user.last_name)
+
+
+
+@receiver(post_save, sender=User)
+def update_user_usuario(sender, instance, created, **kwargs):
+    if created:
+        Usuario.objects.create(user=instance)
+    instance.usuario.save()
